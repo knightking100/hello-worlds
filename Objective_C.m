eaf294d17646e9ca3d1460c304c7de8e99b6b2e0
Shuvikash Patel <Shuvikash.skp@gmail.com>
2017-10-01 21:46:01 +0530

    Add Objective-C Hello World (cherry picked from commit aff5a45d0f9a133d950d2b3eeed3953c477a1337)

---
+++ Objective_C.m
@@ -0,0 +1,11 @@
+// Hello world in Objective C
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+        NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+        NSLog (@"Hello World");
+        [pool drain];
+        return 0;
+}
