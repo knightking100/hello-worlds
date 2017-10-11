cfba6a23ebf88dbc3403323653cf894c3e08fcdb
uwx <chris331hansen@outlook.com>
2017-10-11 12:36:23 -0300

    Update Java_LeshchenkoHello.java

--- Java_LeshchenkoHello.java
+++ Java_LeshchenkoHello.java
@@ -1,7 +1,7 @@
 public class Hacktoberfest {
 
     public static void main(String[] args){
-        System.out.println('Happy Hacktoberfest');
+        System.out.println("Happy Hacktoberfest");
     }
 
 }
