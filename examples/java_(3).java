d42f0dae96e2ee4db6ac93fecad2bad6b6941beb
uwx <chris331hansen@outlook.com>
2017-10-14 11:50:26 -0300

    Update and rename java_(3).java to java_3.java

--- examples/java_(3).java
+++
@@ -1,6 +0,0 @@
-public class java{
-    static{
-        System.out.println("Hello World");
-    }
-    public static void main(String[] args){}
-}
---
+++ examples/java_3.java
@@ -0,0 +1,6 @@
+public class Java {
+    static {
+        System.out.println("Hello World");
+    }
+    public static void main(String[] args) {}
+}
