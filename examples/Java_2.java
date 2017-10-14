bffa83370f27068443a67b2fb1ea41d0853d5357
Raneesh Gomez <raneesh2307@gmail.com>
2017-10-14 10:01:23 +0530

    Added Java_2.java file which displays "Hello World!" using a static method

---
+++ examples/Java_2.java
@@ -0,0 +1,10 @@
+public class Java_2 {
+
+    public static void main(String[] args) {
+        display();
+    }
+
+    public static void display() {
+        System.out.println("Hello World!");
+    }
+}
