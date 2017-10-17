3b91b1bc9887e1bf38ca68f485b4872242363da3
DLGesus <chrisdelagarza@verizon.net>
2017-10-17 14:00:45 -0500

    Create Java_44.java
    My version of helloWorld is a twist in which its the owo style, HewwoWorld

---
+++ examples/Java_44.java
@@ -0,0 +1,10 @@
+public class Java_44 {
+
+    public static void main(String[] args) {
+        HewwoWorld();
+    }
+
+    public static void hewwoWorld() {
+        System.out.println("Hewwo World!");
+    }
+}
