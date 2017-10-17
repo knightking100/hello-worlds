2cfef3221ba86bdaa09852ccc28c7d43ffefd2d4
Tomzk <97286754@qq.com>
2017-10-17 18:42:24 +0800

    Added Hello World (Java)

---
+++ examples/Java_7.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Tom on 2017-10-17.
+ */
+public class Java_7 {
+    public Java_7(){
+        System.out.print("Hello World");
+    }
+    public static void main(String[] args) {
+        Java_7 java_7 = new Java_7();
+    }
+}
