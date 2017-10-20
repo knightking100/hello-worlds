3b735a34f2c6d8f0ed8a253b6ce4d2910f7581c5
Rohit Patil <rohts.patil@gmail.com>
2017-10-21 00:22:19 +0530

    Create TensorFlow.py (#318)
    * Create TensorFlow.py
    
    * Rename Python_4.py to Python_5.py
    
    * Rename Python_5.py to Python_6.py
    
    * Rename Python_6.py to Python_7.py

---
+++ examples/Python_7.py
@@ -0,0 +1,18 @@
+from __future__ import print_function
+
+import tensorflow as tf
+
+# Simple hello world using TensorFlow
+
+# Create a Constant op
+# The op is added as a node to the default graph.
+#
+# The value returned by the constructor represents the output
+# of the Constant op.
+hello = tf.constant('Hello, TensorFlow!')
+
+# Start tf session
+sess = tf.Session()
+
+# Run the op
+print(sess.run(hello))
