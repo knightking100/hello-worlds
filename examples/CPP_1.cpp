bab2a8076780dc3d383c4d10adf9c7152d828107
ZatinMe <30550654+ZatinMe@users.noreply.github.com>
2017-10-24 22:12:26 +0530

    Creating CPP_1.cpp
    This is a CPP file for simple hello world  Command

---
+++ examples/CPP_1.cpp
@@ -0,0 +1,17 @@
+//to include all the library include bits/stdc+.h
+#include <bits/stdc++.h>
+
+//we use namespace as std 
+using namespace std;
+
+//this is where the code starts on c/cpp
+int main()
+{
+//print staement in cpp is cout<<"<the text>"
+
+  cout<<"Hello World!"<<endl;
+
+
+//returning '0' to the main function to ensure a value(integer) is returned.
+return 0;
+}
