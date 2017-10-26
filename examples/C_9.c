a19a56d345de72e127173518bba4ea4acbc34065
Shubham Shirpurkar <shirpurkar.shubham@gmail.com>
2017-10-26 14:28:38 +0530

    Creating C_9.c
    Adding a new way of printing "Hello World!" using C.

---
+++ examples/C_9.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void main()
+{
+	//initialising the array
+	int i,a[]={72,101,108,108,111,32,87,111,114,108,100,33};
+	
+	//running the loop
+	for(i=0;i<12;i++)
+	printf("%c",a[i]);
+
+}
