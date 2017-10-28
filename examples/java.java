133f00bafdaa8bfb255d544e8acbdd279a4ef9bf
Nida <24700264+Nidakhan786@users.noreply.github.com>
2017-10-28 12:09:05 +0530

    Create java.java

---
+++ examples/java.java
@@ -0,0 +1,11 @@
+
+public class Java {
+
+    public static void main(String[] args) {
+        display();
+    }
+
+    public static void display() {
+        System.out.println("Hello World!");
+    }
+}
