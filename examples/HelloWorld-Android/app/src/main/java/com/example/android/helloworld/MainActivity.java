d926441e41e05fe8423eea3c468ba72cded8170f
Saipul Hidayat <the.gum09@gmail.com>
2017-10-31 06:31:28 +0700

    add text with java

--- examples/HelloWorld-Android/app/src/main/java/com/example/android/helloworld/MainActivity.java
+++ examples/HelloWorld-Android/app/src/main/java/com/example/android/helloworld/MainActivity.java
@@ -5,9 +5,13 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView mTextView;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTextView = (TextView) findViewById(R.id.hello_word);
+        mTextView.setText("Welcome Back :)");
     }
 }
