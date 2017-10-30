383e0d0806eab52bd43dfbc70799bb95e84bf5e4
uwx <chris331hansen@outlook.com>
2017-10-30 12:00:12 -0200

    Delete hello.rkt

--- examples/hello.rkt
+++
@@ -1,8 +0,0 @@
-#lang racket
-(define (upcase-all in)
-    (for ([l (in-lines in)])
-      (display (string-upcase l))
-      (newline)))
-(upcase-all (open-input-string
-               (string-append
-                "Hello, World!\n")))
