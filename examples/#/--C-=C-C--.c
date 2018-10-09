86a9318b471e788d023668e95ecfad862fb04b4b
Alexandr Sidorenko <MacOSO@users.noreply.github.com>
2018-10-09 11:39:02 +0700

    Create --C-=C-C--

---
+++ examples/#/--C-=C-C--.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+	int a = 108, b = 111;
+	putchar(72);
+	putchar(101);
+	putchar(a);
+	putchar(a);
+	putchar(b);
+	putchar(32);
+	putchar(87);
+	putchar(b);
+	putchar(114);
+	putchar(a);
+	putchar(100);
+	putchar(33);
+	return 0;
+};
