ef8a94594b7272c21305f304e8ef6e4aed0a1767
Arsalan Diponegoro <arsalan.dp@gmail.com>
2020-10-15 16:43:36 +0700

    add assembly linux (#885)
    * add assembly linux
    
    * Delete Assembly.asm
    
    * Rename AssemblyNasm.asm to Assembly.asm
    
    Co-authored-by: uwx <maxine@cybergal.com>

--- examples/a/Assembly.asm
+++ examples/a/Assembly.asm
@@ -1,31 +1,29 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1561
-{\fonttbl\f0\fmodern\fcharset0 Courier;\f1\froman\fcharset0 Times-Roman;}
-{\colortbl;\red255\green255\blue255;\red0\green0\blue0;}
-{\*\expandedcolortbl;;\cssrgb\c0\c0\c0;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww16480\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\partightenfactor0
+segment .text
+global _start
 
-\f0\fs26 \cf0 \expnd0\expndtw0\kerning0
-section     .text\
-global      _start                              ;must be declared for linker (ld)\
-\
-_start:                                         ;tell linker entry point\
-\
-    mov     edx,len                             ;message length\
-    mov     ecx,msg                             ;message to write\
-    mov     ebx,1                               ;file descriptor (stdout)\
-    mov     eax,4                               ;system call number (sys_write)\
-    int     0x80                                ;call kernel\
-\
-    mov     eax,1                               ;system call number (sys_exit)\
-    int     0x80                                ;call kernel\
-\
-section     .data\
-\
-msg     db  'Hello, world!',0xa                 ;our dear string\
-len     equ $ - msg                             ;length of our dear string\
-\pard\pardeftab720\partightenfactor0
 
-\f1\fs32 \cf0 \
-}
+section .data
+xx db 'Hello world', 0xa
+len equ $ - xx
+
+section .text
+
+_start:
+	mov rax, 0
+	mov rcx, 0
+	mov rbx, 0
+	mov rsi, 0
+	mov rdx, 0
+
+	mov rax, 1
+	mov edx, len
+	mov rsi, xx
+	mov rdi, 1
+	syscall
+
+	mov rax, 60
+	xor rdi, rdi
+	syscall
+
+
+
